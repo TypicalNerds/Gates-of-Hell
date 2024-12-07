c8af09746a2981da8bebe413986778cd4e952ad5
Typical Nerds <107684121+TypicalNerds@users.noreply.github.com>
2024-12-07 13:51:57 +0000

    Update redirect.js
    changed redirect to goto youtube instead of google maps to avoid infinitely getting stuck in a redirect loop on mobile devices

--- js/redirect.js
+++ js/redirect.js
@@ -2,6 +2,6 @@ function playVideo(){
     var video = document.getElementById('video');
     video.play();
     video.addEventListener('ended',function(){
-        window.location = 'https://maps.app.goo.gl/QSncJt6FHUwK99iQ6';
+        window.location = 'https://www.youtube.com/watch?v=HNApxhvK1Hg';
     });
 }
